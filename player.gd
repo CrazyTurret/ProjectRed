7792cc46a5013eec4e743db2d5b18825ee46d6d8
Crazy Turret <crazyturret@gmail.com>
2020-09-09 02:24:45 -0400

    fixed issue #1, updated collision box on player, removed projectile code from player.gd

--- player.gd
+++ player.gd
@@ -1,13 +1,5 @@
 extends KinematicBody2D
 
-
-export (float) var firerate = 1
-export (int) var projspeed = 1
-export (int) var projsize = 1
-export (float) var accuracy = .5
-
-var cooldown = 0
-
 export (int) var speed = 200
 
 var velocity = Vector2()
@@ -49,38 +41,8 @@ func get_input_UD():
 			return 0
 
 
-func fire_projectile(psize,pspeed):
-	var mousepos = get_global_mouse_position()
-	var curpos = global_position
-	var curdir = mousepos - curpos
-	
-	curdir = curdir.normalized()
-	curdir.x += rand_range(-1,1) * (accuracy - 1)
-	curdir.y += rand_range(-1,1) * (accuracy - 1)
-	curdir = curdir.normalized()
-	
-#	var bul = preload("res://Bullet.tscn").instance()
-#	bul.init(pspeed, psize,curdir.normalized())
-#	bul.position = global_position
-#	get_parent().add_child(bul)
-	
-	
-
 func _physics_process(delta):
 	velocity = move_and_slide(velocity)
-	
-	if cooldown > 0:
-		cooldown -= delta
-	if cooldown < 0:
-		cooldown = 0
-	
-	if Input.is_action_pressed("action1"):
-		if cooldown == 0:
-			fire_projectile(projsize,projspeed)
-			cooldown = firerate
-
-func _unhandled_key_input(event):
-	if event.echo == false:
-		velocity.y = get_input_UD()
-		velocity.x = get_input_LR()
-		velocity = velocity.normalized() * speed
+	velocity.y = get_input_UD()
+	velocity.x = get_input_LR()
+	velocity = velocity.normalized() * speed
